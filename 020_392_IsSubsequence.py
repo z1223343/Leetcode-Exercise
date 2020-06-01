eb14be673893f86fc4c3f9b5ed69bd18cfa4fb6b
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-05-31 23:46:58 -0500

    Create 020_392_IsSubsequence.py

---
+++ 020_392_IsSubsequence.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+
+
+"""
+Bisect 库介绍
+是用来二分查找（插入的）
+
+总的来说，bisect.bisect_left bisect.bisect_right
+
+
+https://blog.csdn.net/sinat_38682860/article/details/96438515?utm_medium=distribute.pc_relevant.none-task-blog-baidujs-2
+"""
