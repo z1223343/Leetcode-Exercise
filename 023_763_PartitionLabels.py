5ffadfadd063771849ec0540b231166c3575d907
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-06 23:48:53 -0500

    Update 023_763_PartitionLabels.py

--- 023_763_PartitionLabels.py
+++ 023_763_PartitionLabels.py
@@ -1,6 +1,25 @@
 """
 1 solution
-1. Greedy
+1. Greedy         不是典型的greedy，但是很新颖，useful.
 
 time: O(N)  space: O(N)
 """
+
+
+class Solution(object):
+    def partitionLabels(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        last = {char: index for index, char in enumerate(S)} # dict创建
+        archor = 0
+        ans = []
+        j = 0
+
+        for index, char in enumerate(S):
+            j = max(j, last[char])
+            if j == index:
+                ans.append(j + 1 - archor)
+                archor = j + 1
+        return ans
