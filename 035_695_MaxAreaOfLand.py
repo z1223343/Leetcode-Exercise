c50002c3fff3af95febcd83f61d61b84d633e06d
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-14 22:51:02 -0500

    Update 035_695_MaxAreaOfLand.py

--- 035_695_MaxAreaOfLand.py
+++ 035_695_MaxAreaOfLand.py
@@ -1,7 +1,13 @@
 """
+2 level solution:
+1. DFS (Depth First Search) (Recursive)
+2. DFS (Iterative) (用stack)
 
+   time space
+1. O(R*C)  O(R*C)
+2. O(R*C)  O(R*C)
 """
-
+# solution 1
 class Solution(object):
     def maxAreaOfIsland(self, grid):
         """
@@ -26,6 +32,33 @@ class Solution(object):
             for j in range(c0):
                 areas.append(area(i, j))
         return max(areas)
+
+# solution 2
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        ans = 0
+        seen = set()
+
+        for r0 in range(len(grid)):
+            for c0 in range(len(grid[0])):
+                if grid[r0][c0] == 1 and ((r0, c0) not in seen):
+                    stack = [(r0, c0)]
+                    seen.add((r0, c0))
+                    shape = 0
+                    while stack:
+                        r, c = stack.pop()
+                        shape += 1
+                        for r1, c1 in ((r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1)):
+                            if (0 <= r1 < len(grid)) and (0 <= c1 < len(grid[0])) and grid[r1][c1] == 1 and (
+                                    (r1, c1) not in seen):
+                                stack.append((r1, c1))
+                                seen.add((r1, c1))
+                    ans = max(ans, shape)
+        return ans
 
 """
 
