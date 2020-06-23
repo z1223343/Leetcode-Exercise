31dd8b027b1c13e34ec3c52af956fc0054de0c23
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-23 00:13:35 -0500

    Update 048_40_ComninationSumII.py

--- 048_40_ComninationSumII.py
+++ 048_40_ComninationSumII.py
@@ -1,3 +1,28 @@
 """
-
+1 solution:
+1. Backtracking
 """
+
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def backtrack(first=0,curr=[]):
+            if sum(curr) == target:
+                res.append(curr[:])
+                return
+            if sum(curr) > target:
+                return
+            for i in range(first,len(candidates)):
+                if candidates[i] == candidates[i-1] and i-1>=first: # 我差点儿自己都不知道为什么自己写的code可行
+                    continue
+                curr.append(candidates[i])
+                backtrack(i+1,curr)
+                curr.pop()
+        candidates.sort()
+        res = []
+        backtrack()
+        return res
