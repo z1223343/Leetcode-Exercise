35a7fc11c7285d150e9878f3cde9e03c0f302fee
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-24 23:36:43 -0500

    Create 056_198_HouseRobber.py

---
+++ 056_198_HouseRobber.py
@@ -0,0 +1,17 @@
+"""
+1 solution:
+1. Dynamic programming
+
+time: O(n)   space: O(1)
+"""
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        pre1 = 0
+        pre2 = 0
+        curr = 0
+        for i in range(len(nums)):
+            curr = max(pre2+nums[i],pre1)
+            pre2 = pre1
+            pre1 = curr
+        return curr
