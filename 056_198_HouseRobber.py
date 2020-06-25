da6b495b48af8ad260d5665c9e7b379a2e2328e5
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-25 15:11:19 -0500

    Update 056_198_HouseRobber.py

--- 056_198_HouseRobber.py
+++ 056_198_HouseRobber.py
@@ -11,7 +11,7 @@ class Solution:
         pre2 = 0
         curr = 0
         for i in range(len(nums)):
-            curr = max(pre2+nums[i],pre1)
+            curr = max(pre2+nums[i],pre1)  # 思想牛的
             pre2 = pre1
             pre1 = curr
         return curr
