3de53a288e3d2ec185cbcc949c51c25f0f40a34c
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-26 22:00:35 -0500

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,2 +1,13 @@
-for i in range(5,1,-1):
-    print(i)
+dp1 = [[[1] for j in range(3)] for i in range(2)]
+print(dp1)
+
+dp2 = [[[] for i in range(3)] for i in range(2)]
+print(dp2)
+
+dp3 = [0]* 3
+print(dp3)
+
+dp4 = [[0]*3 for i in range(2)]
+print(dp4)
+
+print([1]+[2])
