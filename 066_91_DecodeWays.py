86dc0fca1087f908e6183e6614a49bf2539b2c3a
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-06-28 00:57:23 -0500

    Update 066_91_DecodeWays.py

--- 066_91_DecodeWays.py
+++ 066_91_DecodeWays.py
@@ -4,9 +4,67 @@
 """
 
 # solution 2 (DP思想是真丫的牛逼)
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 1 if s[0]!='0' else 0
+        for i in range(2,len(s)+1):
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            two_digit = int(s[i-2:i])
+            if two_digit<27 and two_digit>=10:  # 注意啊这里<10不行，容易漏了
+                dp[i] += dp[i-2]
+        return dp[-1]
+# 牛逼，脑子快想烂了。
 
+# 耀中改：这样更 straight-foward点，s[0]=='0'不用试别的了。 循环前面这么改：
+        if s[0] == '0':
+            return 0
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 1
 
+# 耀中再改: 如果你非要dp[i] 管0~i原数组的组合方式（原解法是0~i-1） 也可以的，则code应改为:
+# 为了把这个dp[1]提前算出来，要好多个if啊，不如答案那种简洁
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s[0] == '0':
+            return 0
+        if len(s) == 1:
+            return 1
+        dp = [0] * len(s)
+        dp[0] = 1
+        two_digit = int(s[0:2])
+        if two_digit <= 26:
+            if s[1] == '0':
+                dp[1] = 1
+            else:
+                dp[1] = 2
+        else:
+            if s[1] == '0':
+                dp[1] = 0
+            else:
+                dp[1] = 1
 
+        for i in range(2, len(s)):
+            if s[i] != '0':
+                dp[i] += dp[i - 1]
+            two_digit = int(s[i - 1:i + 1])
+            if two_digit >= 10 and two_digit <= 26:
+                dp[i] += dp[i - 2]
+
+        return dp[-1]
+
+# 其实我发现，如果有一位dp[i] == 0，则答案肯定==0。而且此时一定是有0。
 
 
 """
