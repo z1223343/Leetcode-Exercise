fc60536be176a79f20965d333daca547c3081d91
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-07-07 22:50:48 -0500

    Update trash.py

--- trash.py
+++ trash.py
@@ -1,15 +1,8 @@
-dp1 = [[[1] for j in range(3)] for i in range(2)]
-print(dp1)
-
-dp2 = [[[] for i in range(3)] for i in range(2)]
-print(dp2)
-
-dp3 = [0]* 3
-print(dp3)
-
-dp4 = [[0]*3 for i in range(2)]
-print(dp4)
-
-print([1]+[2])
-
-aoligei
+a = '1234523456'
+print(a.find('2'))
+print(a.find('2',1))
+print(a.find('2',2))
+ans:
+1
+1
+5
