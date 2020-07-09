4caae8a0070887bd5c3df1c4fbffb3ba30b4d61f
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-07-09 00:13:03 -0500

    Update 071_416_PartitionEqualSubsetSum.py

--- 071_416_PartitionEqualSubsetSum.py
+++ 071_416_PartitionEqualSubsetSum.py
@@ -1,6 +1,6 @@
 """
 1 level solution:
-1. DP
+1. DP (knapsack problem)
 """
 
 # solution 1. (想想感觉是逻辑是对的，再想想又不知道它为什么是对的，但自己想的话可能想不出来这么解，牛逼。
@@ -16,3 +16,4 @@ class Solution:
             for j in range(W,i-1,-1): # 就这一行，全是细节
                 dp[j] = dp[j] or dp[j-i]
         return dp[W]
+# 有更快的解法，好像是DFS，先欠着
