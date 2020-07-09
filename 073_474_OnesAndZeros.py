95811574e963a0f438826ff7ce938eb429db0bed
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-07-09 00:13:10 -0500

    Create 073_474_OnesAndZeros.py

---
+++ 073_474_OnesAndZeros.py
@@ -0,0 +1,28 @@
+"""
+1 level solution:
+1. DP (knapsack problem)
+
+time:  O()    space:  O()
+"""
+
+# solution 1:
+# 多维费用01背包问题
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        def count01(string):
+            count_1 = 0
+            count_0 = 0
+            for i in range(0, len(string)):
+                if string[i] == '0':
+                    count_0 += 1
+                else:
+                    count_1 += 1
+            return count_0, count_1
+
+        dp = [[0] * (m + 1) for i in range(0, n + 1)]
+        for string in strs:
+            count0, count1 = count01(string)
+            for i in range(m, count0 - 1, -1):
+                for j in range(n, count1 - 1, -1):
+                    dp[j][i] = max(dp[j][i], 1 + dp[j - count1][i - count0])
+        return dp[n][m]
