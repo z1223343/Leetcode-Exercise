9520ac1d6350f7f86803d2c5bd84b0c320b253c7
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-07-08 21:12:45 -0500

    Create 071_416_PartitionEqualSubsetSum.py

---
+++ 071_416_PartitionEqualSubsetSum.py
@@ -0,0 +1,18 @@
+"""
+1 level solution:
+1. DP
+"""
+
+# solution 1. (想想感觉是逻辑是对的，再想想又不知道它为什么是对的，但自己想的话可能想不出来这么解，牛逼。
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        sum_list = sum(nums)
+        if sum_list%2 != 0:
+            return False
+        W = sum_list//2
+        dp = [False]*(W+1)
+        dp[0] = True
+        for i in nums:
+            for j in range(W,i-1,-1): # 就这一行，全是细节
+                dp[j] = dp[j] or dp[j-i]
+        return dp[W]
