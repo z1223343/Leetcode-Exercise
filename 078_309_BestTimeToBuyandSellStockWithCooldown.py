19ba7fc6f8a29c592ea7247b96356b6b7b38415c
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-07-23 23:01:53 -0500

    Update 078_309_BestTimeToBuyandSellStockWithCooldown.py

--- 078_309_BestTimeToBuyandSellStockWithCooldown.py
+++ 078_309_BestTimeToBuyandSellStockWithCooldown.py
@@ -1,14 +1,20 @@
 """
+1 level solution:
+1. DP (with state machine)
 
+    time   space
+1.  O(N)    O(1)
 """
 
 # soulution 1
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        sold, held, reset = float('-inf'), float('-inf'), 0
+        sold = float('-inf')
+        held = float('-inf')
+        reset = 0
         for price in prices:
             pre_sold = sold
-            sold = held+price
+            sold = held + price
             held = max(held,reset-price)
             reset = max(reset,pre_sold)
         return max(sold,reset)
