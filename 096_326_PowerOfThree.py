70cda8aa0719e673630b8d1bf7ce244ba9a19a06
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-08-02 00:00:16 -0500

    Update 096_326_PowerOfThree.py

--- 096_326_PowerOfThree.py
+++ 096_326_PowerOfThree.py
@@ -1,4 +1,30 @@
 """
+2 level solutions:
+1. Loop iteration (正常做法）
+2. Interger limitation （牛逼做法）
 
-
+    time       space
+1. O(log3(n))  O(1)
+2.  O(1)       O(1)
 """
+# solution 1:
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<1:
+            return False
+        while n%3 == 0:
+            n/=3
+        return n==1
+
+
+# solution 2:
+import math
+maxint = 2**32/2-1
+maxpower = int(math.floor(math.log(maxint,3)))
+maxpowerof3 = 3**maxpower
+print(maxint,maxpower,maxpowerof3)
+
+# maxpowerof3 = 1162261467
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return (n>0) and (1162261467%n==0)
