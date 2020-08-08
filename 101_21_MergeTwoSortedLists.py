c8f087114b9bb6cbd9725838af23dab80fa18b52
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-08-07 23:54:29 -0500

    Create 101_21_MergeTwoSortedLists.py

---
+++ 101_21_MergeTwoSortedLists.py
@@ -0,0 +1,28 @@
+"""
+1 level solution
+1. iteration
+
+time: O(m+n)  space: O(1)
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1    # note 这里把l1都挂上
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 is not None else l2
+        return prehead.next
