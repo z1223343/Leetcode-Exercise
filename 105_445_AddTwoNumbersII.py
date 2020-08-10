fa147a2e9f5cd64be813bbf6fb04868484e3610a
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-08-09 23:45:25 -0500

    Create 105_445_AddTwoNumbersII.py

---
+++ 105_445_AddTwoNumbersII.py
@@ -0,0 +1,30 @@
+"""
+1 level solution:
+1. convert to number and then create new listnode
+"""
+
+# solution 1:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        n1 = 0
+        n2 = 0
+        while l1:
+            n1 = n1 * 10 + l1.val
+            l1 = l1.next
+        while l2:
+            n2 = n2 * 10 + l2.val
+            l2 = l2.next
+        n3 = n1 + n2
+
+        if n3 == 0:
+            return ListNode(0, None)
+        result = None
+        while n3:
+            result = ListNode(n3 % 10, result)
+            n3 //= 10
+        return result
