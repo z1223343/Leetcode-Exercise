143d2bf2da79994bc3a30a0bbbe225fb71b3b5c2
z1223343 <39869876+z1223343@users.noreply.github.com>
2020-08-10 23:37:18 -0500

    Update 100_206_ReverseLinkedList.py

--- 100_206_ReverseLinkedList.py
+++ 100_206_ReverseLinkedList.py
@@ -20,3 +20,17 @@ class Solution:
             prev = curr
             curr = nextTmp
         return prev
+
+# solution 2：
+# csdn 看到的野路子牛逼解法，对连续赋值有更深的理解
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        L= ListNode(float('-inf'))
+        while head:
+            L.next,head.next,head = head,L.next,head.next
+        return L.next
