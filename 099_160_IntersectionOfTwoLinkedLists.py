0c1c1f633c4ceb4cc9950340397403628a88d5ed
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-01 15:20:07 -0600

    Update 099_160_IntersectionOfTwoLinkedLists.py

--- 099_160_IntersectionOfTwoLinkedLists.py
+++ 099_160_IntersectionOfTwoLinkedLists.py
@@ -16,6 +16,6 @@ class Solution:
         l1 = headA
         l2 = headB
         while l1!=l2:
-            l1 = l1.next if l1!=None else headB
+            l1 = l1.next if l1!=None else headB  # 注意这里l1.next!=None就不行了，因为如果headAB不一样长，那么如何没有交叉点会无限循环下去，而如果headAB一样长的话，就循环一个长度就行了，不需要A->B B->A调换，综上l1!=None正确。
             l2 = l2.next if l2!=None else headA
         return l1
