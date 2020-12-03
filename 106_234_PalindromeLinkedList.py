ba7290af7183a9993ed04eafa13c2311e2b8b8d3
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-02 23:34:38 -0600

    Update 106_234_PalindromeLinkedList.py

--- 106_234_PalindromeLinkedList.py
+++ 106_234_PalindromeLinkedList.py
@@ -56,3 +56,42 @@ class Solution:
             prev = curr
             curr = tmp
         return prev
+
+
+# solution 1 (2刷自己写的，没有保证input原链表保持不变）： 这里如果input is [1,2,3,4,5,6,7,8,9]，那么运行完head会变成[1,2,3,4,5,6].原因在rever_nodes这个函数，因为第一个head.next = L.next，让6之后的node都删除了，后面的循环并不影响我们的原始input，具体为什么自己想把，我想了15分钟想明白了。
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+
+        if not head:
+            return True
+
+        first_half_nodes = self.get_first_half_nodes(head)
+        reversed_second_half_nodes = self.reverse_nodes(first_half_nodes.next)
+
+        first_posi = head # cannot use first_half_nodes
+        second_posi = reversed_second_half_nodes
+        while second_posi:  # note it should be second_posi instead of first_posi, because length(first_half)>=second_half, also first is actually head here.
+            if first_posi.val != second_posi.val:
+                return False
+            else:
+                first_posi = first_posi.next
+                second_posi = second_posi.next
+        return True
+
+    def get_first_half_nodes(self, head):
+        first = head
+        second = head
+        while first.next and first.next.next:
+            first = first.next.next
+            second = second.next
+        return second
+
+    def reverse_nodes(self, head):
+        L = ListNode(-1)
+        while head:
+            L.next, head.nex
