e1dc32be79e4426048aa113ee1c9884c9ea6cd77
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-24 23:47:08 -0600

    Create 109_104_MaximumDepthOfBinaryTree.py

---
+++ 109_104_MaximumDepthOfBinaryTree.py
@@ -0,0 +1,22 @@
+"""
+1. recursion
+2. iteration
+
+"""
+
+# solution 2:
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+
+        return depth
