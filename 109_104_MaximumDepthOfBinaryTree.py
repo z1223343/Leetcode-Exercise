b796a528003c3500143cb53396f3e5fe59445041
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-26 02:08:41 -0600

    Update 109_104_MaximumDepthOfBinaryTree.py

--- 109_104_MaximumDepthOfBinaryTree.py
+++ 109_104_MaximumDepthOfBinaryTree.py
@@ -4,8 +4,15 @@
 
 """
 
-# solution 1:
-
+# solution 1: (DFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left = self.maxDepth(root.left)
+            right = self.maxDepth(root.right)
+            return max(left,right)+1
 
 
 # solution 2:
@@ -16,7 +23,7 @@ class Solution:
             stack.append((1, root))
 
         depth = 0
-        while stack != []:
+        while stack != []: # note "stack is not None" 就错了。 [] is not None.
             current_depth, root = stack.pop()
             if root is not None:
                 depth = max(depth, current_depth)
@@ -24,3 +31,7 @@ class Solution:
                 stack.append((current_depth + 1, root.right))
 
         return depth
+
+"""
+这个方法要注意的是，如果stack.append((a,b)),可以直接 x,y = stack.pop()
+"""
