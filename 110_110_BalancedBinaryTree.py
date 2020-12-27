94d2c6583cc80a7bd5cc2cefec2308234efa57bf
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-27 15:44:54 -0600

    Update 110_110_BalancedBinaryTree.py

--- 110_110_BalancedBinaryTree.py
+++ 110_110_BalancedBinaryTree.py
@@ -1,8 +1,27 @@
 """
-1. recursion
-2. iteration
+1. top-down recursion
+2. bottom-up recursion
 
      time   space
-1.   O(N)    O(logN)
-2.   O(N)    O(logN)
-"""sssssss
+1.   O(N)    O(NlogN)
+2.   O(N)    O(N)
+"""
+
+# solution 1:
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        else:
+            ans = abs(self.height(root.left)-self.height(root.right))<2
+            return ans and self.isBalanced(root.left) and self.isBalanced(root.right) # 但是这个样子啊，好像会造成多余的计算，最理想的情况是有一个sub tree判为不平衡就可以直接给出最终答案了。
+    def height(self,root: TreeNode) -> int:
+        if root is None:
+            return -1
+        else:
+            return 1+max(self.height(root.left),self.height(root.right))
+
+
+
+
+# solution 2:
