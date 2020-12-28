c8f577be72a0f7db55e724a07df5b6a408066572
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-28 00:10:03 -0600

    Update 111_543_DiameterofBinaryTree.py

--- 111_543_DiameterofBinaryTree.py
+++ 111_543_DiameterofBinaryTree.py
@@ -1,8 +1,21 @@
 """
-1. recursion
-2. iteration
+1. DFS (recursion)
 
      time   space
-1.   O(N)    O(logN)
-2.   O(N)    O(logN)
+1.   O(N)    O(N)
 """
+
+# solution 1:
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 0
+        def depth(root):
+            if not root:
+                return 0
+            else:
+                left = depth(root.left)
+                right = depth(root.right)
+                self.ans = max(left+right,self.ans)
+                return max(left,right)+1
+        depth(root)
+        return self.ans
