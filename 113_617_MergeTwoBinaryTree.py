019ca7328dde44b9c15c03bb421324f46874fa40
Yaozhong <zhangyaozhong66@gmail.com>
2020-12-29 00:26:05 -0600

    Update 113_617_MergeTwoBinaryTree.py

--- 113_617_MergeTwoBinaryTree.py
+++ 113_617_MergeTwoBinaryTree.py
@@ -1,8 +1,20 @@
 """
-1. Recursive
+1. Recursive (DFS)
 
      time   space
 1.   O(N)    O(N)
 """
 
 # solution 1:
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+
+        if t1 is None:
+            return t2
+        elif t2 is None:
+            return t1
+        else:
+            t1.val = t1.val + t2.val
+            t1.left = self.mergeTrees(t1.left,t2.left)
+            t1.right = self.mergeTrees(t1.right,t2.right)
+        return t1
